0e4bb63a70d4f30eae63a22d33860ef8d82d8b26
phiarchitect <phi@phiarchitect.com>
2023-11-13 08:46:32 -0800

    use setup_globals in conf

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -6,30 +6,32 @@ org_name = "GEOMETOR"
 repo = "model"
 repo_name = "model"
 
-blog_title = f'{org_name} • {repo_name}'
-html_title = f'{org_name} • {repo_name}'
-project = f'{org_name} • {repo_name}'
-version = ''  # The short X.Y version.
-release = ''  # The full version, including alpha/beta/rc tags.
+setup_globals(org, org_name, repo, repo_name)
 
-copyright = f'{year}, {org_name}'
-author = f'{org_name}'
+#  blog_title = f'{org_name} • {repo_name}'
+#  html_title = f'{org_name} • {repo_name}'
+#  project = f'{org_name} • {repo_name}'
+#  version = ''  # The short X.Y version.
+#  release = ''  # The full version, including alpha/beta/rc tags.
 
-# Base URL for the website, required for generating feeds.
-# e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = f'https://{org}.github.io/{repo}'
-html_base_url = blog_baseurl
-html_baseurl = blog_baseurl
+#  copyright = f'{year}, {org_name}'
+#  author = f'{org_name}'
 
-blog_authors = {
-    "phi": ("phi ARCHITECT", None),
-}
+#  # Base URL for the website, required for generating feeds.
+#  # e.g. blog_baseurl = "http://example.com/"
+#  blog_baseurl = f'https://{org}.github.io/{repo}'
+#  html_base_url = blog_baseurl
+#  html_baseurl = blog_baseurl
 
-html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_user": org, # Username
-    "github_repo": repo, # Repo name
-    "github_version": "main", # Version
-    "conf_py_path": "/docsrc/", # Path in the checkout to the docs root
-}
+#  blog_authors = {
+    #  "phi": ("phi ARCHITECT", None),
+#  }
+
+#  html_context = {
+    #  "display_github": True, # Integrate GitHub
+    #  "github_user": org, # Username
+    #  "github_repo": repo, # Repo name
+    #  "github_version": "main", # Version
+    #  "conf_py_path": "/docsrc/", # Path in the checkout to the docs root
+#  }
 
